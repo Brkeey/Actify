101b27382d1a7ecde53dac1b5327a16a660d0f41
Berke Yılmaz <berke.yilmaz@istun.edu.tr>
2025-02-08 13:32:49 +0300

    Initial Commit

---
+++ Actify/ActifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActifyApp.swift
+//  Actify
+//
+//  Created by Berke Yılmaz on 8.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ActifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActifyTests/ActifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ActifyTests.swift
+//  ActifyTests
+//
+//  Created by Berke Yılmaz on 8.02.2025.
+//
+
+import Testing
+@testable import Actify
+
+struct ActifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
